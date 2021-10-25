08d39941c1548aead9e1c51ee0014410760d34f4
Hiren Vadher <hirenvadher954@gmail.com>
2021-10-25 22:39:27 +0530

    Create Duplicate Zeros.java

---
+++ Duplicate Zeros.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int n = arr.length;
+        for(int i = n-1;i>=0;i--){
+            if (arr[i]==0){
+                for(int j=n-1;j>i;j--){
+                    arr[j] = arr[j-1];
+                }
+            }
+        }
+    }
+}
