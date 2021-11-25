52b5d85489c7e3145d93352f71da1c3efc353d4a
Hiren Vadher <hirenvadher954@gmail.com>
2021-11-25 23:36:57 +0530

    Update Merge Sorted Array.java

--- Merge Sorted Array.java
+++ Merge Sorted Array.java
@@ -4,8 +4,8 @@ class Solution {
         int end1 = m - 1;
         int end2 = n - 1;
         
-        while(end2>=0){
-            if(end1>=0){
+        while(end2>=0) {
+            if(end1>=0) {
                 nums1[end--]=nums1[end1]>nums2[end2]?nums1[end1--] : nums2[end2--];
             }else{
                 nums1[end--]=nums2[end2--];
