d1319183bea1a602a4f6a8e1a93daf32dd5471e8
Hiren Vadher <hirenvadher954@gmail.com>
2021-12-02 23:35:30 +0530

    Update Duplicate Zeros.java

--- Duplicate Zeros.java
+++ Duplicate Zeros.java
@@ -1,6 +1,7 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
         int n = arr.length;
+        
         for(int i = n-1;i>=0;i--){
             if (arr[i]==0){
                 for(int j=n-1;j>i;j--){
