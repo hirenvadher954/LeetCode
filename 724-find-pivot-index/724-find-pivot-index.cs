124ca5e30da402064bc489ec693d9bf58273ec60
Hiren Vadher <hirenvadher954@gmail.com>
2022-09-23 00:44:12 +0530

    Time: 193 ms (39.48%), Space: 39.7 MB (87.24%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int PivotIndex(int[] nums) {
+        int leftSum = 0;
+        int rightSum = nums.Sum();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            rightSum -= nums[i];
+            if (leftSum == rightSum) return i;
+            leftSum += nums[i];
+
+        }
+
+        return -1;
+    }
+}
