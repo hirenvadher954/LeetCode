6cb34a9858dad47c0393d158e592540b1fdd442a
Hiren Vadher <hirenvadher954@gmail.com>
2022-10-05 14:40:30 +0530

    Time: 148 ms (53.36%), Space: 51.9 MB (56.02%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,15 @@
+function maxProfit(prices: number[]): number {
+    let buy: number = prices[0];
+    let max_profits: number = 0;
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < buy)
+            buy = prices[i];
+        else if (prices[i] - buy>= max_profits) {
+            max_profits = prices[i] - buy;
+        }
+    }
+
+    return max_profits;
+
+    return max_profits;
+}
