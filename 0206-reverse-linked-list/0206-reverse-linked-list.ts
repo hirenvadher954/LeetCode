21a3f32b235e7b5f18f7ca8a8dd85830089ca1ac
Hiren Vadher <hirenvadher954@gmail.com>
2022-10-10 22:54:16 +0530

    Time: 128 ms (20.19%), Space: 44.7 MB (85.45%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.ts
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+    let prev = null, next = null;
+    let current = head;
+    while (current != null) {
+        next = current.next;
+        current.next = prev;
+        prev = current
+        current = next
+
+    }
+    return prev;
+}
