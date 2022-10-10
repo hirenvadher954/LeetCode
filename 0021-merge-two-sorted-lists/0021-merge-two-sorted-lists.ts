a0cf42743fb00055bd42908809e73e9b1aa2eeff
Hiren Vadher <hirenvadher954@gmail.com>
2022-10-10 21:49:02 +0530

    Time: 143 ms (15.90%), Space: 44.9 MB (66.88%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.ts
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    let head = new ListNode();
+    let current = head;
+    while (list1 != null && list2 != null) {
+        if (list1.val <= list2.val) {
+            current.next = list1;
+            list1 = list1.next;
+        } else {
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
+    }
+    if (list1 != null) {
+        current.next = list1;
+    }
+    if (list2 != null) {
+        current.next = list2;
+    }
+    return head.next;
+}
