afc3272a59e2b76dc64036f99cbf7662222fba1b
Hiren Vadher <hirenvadher954@gmail.com>
2022-10-11 23:39:22 +0530

    Time: 85 ms (87.45%), Space: 44.8 MB (67.04%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.ts
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.ts
@@ -10,24 +10,17 @@
  * }
  */
 
-function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
-    let head = new ListNode();
-    let current = head;
-    while (list1 != null && list2 != null) {
-        if (list1.val <= list2.val) {
-            current.next = list1;
-            list1 = list1.next;
-        } else {
-            current.next = list2;
-            list2 = list2.next;
-        }
-        current = current.next;
+function mergeTwoLists(list1, list2) {
+    if (list1 == null)
+        return list2;
+    if (list2 == null)
+        return list1;
+    if (list1.val < list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
     }
-    if (list1 != null) {
-        current.next = list1;
+    else {
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
     }
-    if (list2 != null) {
-        current.next = list2;
-    }
-    return head.next;
 }
