e18f6d8ef98c2eb9a57dcf89cff24498fbca52ff
Hiren Vadher <hirenvadher954@gmail.com>
2022-10-13 23:58:49 +0530

    Time: 147 ms (5.54%), Space: 45.2 MB (50.92%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.ts
@@ -0,0 +1,18 @@
+function longestPalindrome(s: string): number {
+    const obj = {};
+    let sum = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const ch = s[i];
+        if (!obj[ch]) {
+            obj[ch] = 1;
+        } else {
+            obj[ch] = 0;
+            sum += 2;
+        }
+    }
+
+    if (sum < s.length)
+        return sum + 1;
+    return sum;
+}
