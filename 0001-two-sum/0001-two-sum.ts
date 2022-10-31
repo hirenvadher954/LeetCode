06d773bcbd5f9d4d2af15260028a42f5f76b3051
Hiren Vadher <hirenvadher954@gmail.com>
2022-10-31 23:54:23 +0530

    Time: 113 ms (76.47%), Space: 45.5 MB (29.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,7 +1,9 @@
 function twoSum(nums: number[], target: number): number[] {
+    let seen = new Map();
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] == target) return [i, j];
+        if(seen.has(target - nums[i])) {
+            return [seen.get(target - nums[i]), i];
         }
+        seen.set(nums[i], i);
     }
 }
