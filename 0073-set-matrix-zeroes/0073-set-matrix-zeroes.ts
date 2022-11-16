bb1a1cfbdfd3a3fd68ca55948f017d6426d1848d
Hiren Vadher <hirenvadher954@gmail.com>
2022-11-16 22:56:46 +0530

    Time: 195 ms (5.39%), Space: 50.7 MB (5.39%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.ts
@@ -0,0 +1,24 @@
+function setZeroes(matrix: number[][]): void {
+    const row = matrix.length;
+    const col = matrix[0].length;
+    let rows_zero = [];
+    let col_zero = [];
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            if (matrix[i][j] == 0) {
+                rows_zero.push(i);
+                col_zero.push(j);
+            }
+        }
+    }
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            if (rows_zero.includes(i)) {
+                matrix[i] = Array(col).fill(0);
+            }
+            if (col_zero.includes(j)) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
