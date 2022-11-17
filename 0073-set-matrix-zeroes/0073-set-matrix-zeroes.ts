f4897b3c5c110f5130ac8825b2214f293e1530d6
Hiren Vadher <hirenvadher954@gmail.com>
2022-11-17 23:01:55 +0530

    Time: 177 ms (12.62%), Space: 45.6 MB (45.63%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.ts
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.ts
@@ -1,21 +1,39 @@
 function setZeroes(matrix: number[][]): void {
-    const row = matrix.length;
-    const col = matrix[0].length;
-    let rows_zero = [];
-    let col_zero = [];
-    for (let i = 0; i < row; i++) {
-        for (let j = 0; j < col; j++) {
-            if (matrix[i][j] == 0) {
-                rows_zero.push(i);
-                col_zero.push(j);
+    const m = matrix.length;
+    const n = matrix[0].length;
+    let first_row_has_zero = false;
+    let first_col_has_zero = false;
+    for (let row = 0; row < m; row++) {
+        for (let col = 0; col < n; col++) {
+            if (matrix[row][col] == 0) {
+                if (row == 0) {
+                    first_row_has_zero = true;
+                }
+                if (col == 0) {
+                    first_col_has_zero = true;
+                }
+                matrix[row][0] = 0;
+                matrix[0][col] = 0;
             }
         }
     }
-    for (let i = 0; i < row; i++) {
-        for (let j = 0; j < col; j++) {
-            if (rows_zero.includes(i) || col_zero.includes(j)) {
-                matrix[i][j] = 0;
+    for (let row = 1; row < m; row++) {
+        for (let col = 1; col < n; col++) {
+            if (matrix[row][0] == 0 || matrix[0][col] == 0) {
+                matrix[row][col] = 0;
             }
+        }
+    }
+
+    if(first_row_has_zero){
+        for (let col = 0; col < n; col++) {
+            matrix[0][col] = 0
+        }
+    }
+
+    if(first_col_has_zero){
+        for (let row = 0; row < m; row++) {
+            matrix[row][0] = 0
         }
     }
 }
