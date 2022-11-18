0eec5b934fb8ccaa2f0cb994f53a0f653c071882
Hiren Vadher <hirenvadher954@gmail.com>
2022-11-18 23:38:49 +0530

    Time: 115 ms (63.23%), Space: 45.2 MB (33.55%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.ts
@@ -0,0 +1,36 @@
+function nextPermutation(nums: number[]): void {
+    let index = -1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (nums[i] > nums[i - 1]) {
+            index = i - 1;
+            break;
+        }
+    }
+
+    if (index === -1) {
+        nums.sort((a, b) => a - b);
+        return;
+    }
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (nums[i] > nums[index]) {
+            swap(i, index);
+            break;
+        }
+    }
+
+    reverse(index + 1);
+
+    function swap(i: number, j: number) {
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+
+    function reverse(start: number) {
+        let left = start, right = nums.length - 1;
+        while (left < right) {
+            swap(left, right);
+            left++;
+            right--;
+        }
+    }
+}
