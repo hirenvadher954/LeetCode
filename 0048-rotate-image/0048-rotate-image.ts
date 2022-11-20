4342b9fb05e8b91ae81ed665130619e0fe2a0bb0
Hiren Vadher <hirenvadher954@gmail.com>
2022-11-20 20:47:43 +0530

    Time: 87 ms (77.95%), Space: 45.6 MB (7.35%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.ts
@@ -0,0 +1,15 @@
+function rotate(matrix: number[][]): void {
+    const n = matrix.length;
+    
+    for (let i = 0; i < n; i++) {
+        for (let j = i; j < n; j++) {
+            console.log(i,j);
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
+    }
+
+    for (let i = 0; i < matrix.length; i++) {
+        matrix[i].reverse();
+    }
+
+}
