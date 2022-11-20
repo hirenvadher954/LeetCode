5e443b12459b4c07e554aa94259594d295c0dca2
Hiren Vadher <hirenvadher954@gmail.com>
2022-11-20 22:32:10 +0530

    Time: 92 ms (95.62%), Space: 51.4 MB (81.02%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.ts
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.ts
@@ -1,14 +1,18 @@
 function findDuplicate(nums: number[]): number {
     let slow = nums[0];
     let fast = nums[0];
+
     do {
         slow = nums[slow];
         fast = nums[nums[fast]];
     } while (slow != fast);
+
     fast = nums[0];
+
     while (slow != fast) {
         slow = nums[slow];
         fast = nums[fast];
     }
+
     return slow;
 }
