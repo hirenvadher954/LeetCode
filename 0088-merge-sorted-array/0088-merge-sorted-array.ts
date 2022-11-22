67bd24be44f3ef946115264039c6525cccb2c783
Hiren Vadher <hirenvadher954@gmail.com>
2022-11-22 22:01:28 +0530

    Time: 120 ms (22.06%), Space: 44.2 MB (54.41%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.ts
+++ 0088-merge-sorted-array/0088-merge-sorted-array.ts
@@ -1,18 +1,32 @@
-/**
- Do not return anything, modify nums1 in-place instead.
- */
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    let i = m -1;
-    let j = n-1;
-    let k = m+n-1;
+    //i is the index of the last element in nums1
+    let i: number = m -1;
+    
+    //j is the index of the last element in nums2
+    let j: number = n-1;
+    
+    //k is the index of the last element in the merged array
+    let k: number = m+n-1;
+    
+    //while both arrays have elements
     while(i>=0&&j>=0){
+        
+        //if the last element of nums1 is greater than the last element of nums2
         if(nums1[i]>nums2[j]){
+            //place the last element of nums1 at the end of the merged array
             nums1[k--] = nums1[i--];
-        }else{
+        }
+        
+        //if the last element of nums2 is greater than the last element of nums1
+        else{
+             //place the last element of nums2 at the end of the merged array
              nums1[k--] = nums2[j--];
         }
     }
+
+    //if nums1 has no more elements
     while(j>=0){
+        //place the remaining elements of nums2 at the beginning of the merged array
         nums1[k--] = nums2[j--];
     }
 };
