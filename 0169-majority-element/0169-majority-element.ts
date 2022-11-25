7be462738b20ebc5f3731809a16936890771be52
Hiren Vadher <hirenvadher954@gmail.com>
2022-11-25 22:55:08 +0530

    Time: 141 ms (17.10%), Space: 46.3 MB (30.18%) - LeetHub

--- 0169-majority-element/0169-majority-element.ts
+++ 0169-majority-element/0169-majority-element.ts
@@ -1,17 +1,15 @@
 function majorityElement(nums: number[]): number {
-    let count = {}
-    let n = nums.length
-    for(let i = 0; i<n;i++){
-        if(count[nums[i]]){
-            count[nums[i]] += 1;
-        }else{
-            count[nums[i]]  = 1;
-        }
+    let counts = {};
+    let n = nums.length;
+    
+    for (const num of nums) {
+        counts[num] = counts[num] ? counts[num] + 1 : 1;
     }
+    
     let major = Math.ceil(n / 2);
-    for(let key in count){
-        if(count[key]>=major){
+    for (let key in counts) {
+        if (counts[key] >= major) {
             return parseInt(key);
         }
     }
-};
+}
