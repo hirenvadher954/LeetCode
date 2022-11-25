ce59bbcdd147b87f14d2b1f340a097a09fc60326
Hiren Vadher <hirenvadher954@gmail.com>
2022-11-25 22:39:47 +0530

    Time: 108 ms (57.09%), Space: 44.5 MB (40.95%) - LeetHub

--- 0050-powx-n/0050-powx-n.ts
+++ 0050-powx-n/0050-powx-n.ts
@@ -1,3 +1,18 @@
+
 function myPow(x: number, n: number): number {
-    return x**n;
-};
+    let ans = 1;
+    let number = Math.abs(n);
+
+    while (number > 0) {
+        if (number % 2 == 1) {
+            ans = ans * x;
+            number -= 1;
+        } else {
+            x = x * x;
+            number /= 2;
+        }
+    }
+
+    if (n < 0) ans = 1 / ans;
+    return ans;
+}
