e8daee6e2c648d379559e02091a805dab907fe9a
Hiren Vadher <hirenvadher954@gmail.com>
2022-11-28 23:58:29 +0530

    Time: 105 ms (57.14%), Space: 42.4 MB (97.08%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.ts
@@ -0,0 +1,9 @@
+function uniquePaths(m: number, n: number): number {
+  let N = n + m - 2;
+  let r = m - 1; 
+  let res = 1;
+
+  for (let i = 1; i <= r; i++)
+      res = res * (N - r + i) / i;
+  return res;
+}
